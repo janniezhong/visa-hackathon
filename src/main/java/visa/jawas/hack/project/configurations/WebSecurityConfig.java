7d64eaf01397dc65d7140589400ed491504bb069
Jannie <jz3191@columbia.edu>
2020-06-24 02:11:32 -0700

    really, really, really basic + ugly login - but it works!

--- src/main/java/visa/jawas/hack/project/configurations/WebSecurityConfig.java
+++ src/main/java/visa/jawas/hack/project/configurations/WebSecurityConfig.java
@@ -2,12 +2,15 @@ package visa.jawas.hack.project.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -28,16 +31,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
-    @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER");
     }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
--- src/main/java/visa/jawas/hack/project/controllers/MainController.java
+++ src/main/java/visa/jawas/hack/project/controllers/MainController.java
@@ -2,26 +2,29 @@ package visa.jawas.hack.project.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
+    @RequestMapping(path = "/", method = RequestMethod.GET)
     public String index(){
         return("home");
     }
-    @GetMapping("/home")
+
+    @RequestMapping(path = "/home", method = RequestMethod.GET)
     public String home(){
         return("home");
     }
-//    @GetMapping("/login")
-//    public String login(){
-//        return("login");
-//    }
-//    @GetMapping("/")
-//    public String hello(){
-//        return("hello");
-//    }
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String login(){
+        return("login");
+    }
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+    public String hello(){
+        return("hello");
+    }
 
 
 }
